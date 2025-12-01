d182137bc05bb7ef4c68056dffec6e1fbd161e92
Linus Fischer <leicraft@leicraftmc.de>
2025-12-01 19:40:58 +0000

    update

---
+++ openapi-ts.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from '@hey-api/openapi-ts';
+
+export default defineConfig({
+    input: 'https://www.aptly.info/swagger/aptly_1.6.2.json',
+    output: 'src/aptly/api-client',
+    plugins: [
+        '@hey-api/client-nuxt'
+    ]
+});
